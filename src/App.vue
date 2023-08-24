40ea0227b10c3bbbd7e3583279b791faee142d24
Mehedi Jaman <mail4mjaman@gmail.com>
2023-08-24 23:43:57 +0600

    Initial Commit

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<script setup>
+import {ref} from 'vue'
+import axios from 'axios'
+
+const baseUrl = 'https://jsonplaceholder.typicode.com/posts'
+const weatherData = ref('')
+
+// async function getWeather(){
+//   const data = await axios.get(baseUrl)
+//   weatherData.value = data
+// }
+
+function getWeather(){
+  axios.get(baseUrl)
+  .then(res => {
+    weatherData.value = res
+  })
+}
+
+getWeather()
+</script>
+
+<template>
+{{ weatherData.data }}
+</template>
